b10fac3c4167fc50a7a5f02163a8538c0a743795
Stefan Bielak <sbielak20@gmail.com>
2023-08-31 19:14:47 +0200

    zadanie nr.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const feedbackForm = document.querySelector('.feedback-form');
+  const emailInput = feedbackForm.querySelector('[name="email"]');
+  const messageInput = feedbackForm.querySelector('[name="message"]');
+  const saveFormStateThrottled = _.throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }, 500);
+  emailInput.addEventListener('input', saveFormStateThrottled);
+  messageInput.addEventListener('input', saveFormStateThrottled);
+  const storedFormState = localStorage.getItem('feedback-form-state');
+  if (storedFormState) {
+    const parsedFormState = JSON.parse(storedFormState);
+    emailInput.value = parsedFormState.email;
+    messageInput.value = parsedFormState.message;
+  }
+  feedbackForm.addEventListener('submit', event => {
+    event.preventDefault();
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
+    console.log('Form data submitted:');
+    console.log('Email:', parsedFormState.email);
+    console.log('Message:', parsedFormState.message);
+  });
+});
