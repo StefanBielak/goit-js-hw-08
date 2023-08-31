d61b96b853bf10678f57ab7ae78016035c7a2d36
Stefan Bielak <sbielak20@gmail.com>
2023-08-31 19:08:54 +0200

    zadanie nr 2 aktualizacja

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,46 @@
 
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+    const vimeoPlayer = document.getElementById("vimeo-player");
+    const savedTime = localStorage.getItem("vimeoPlaybackTime");
+    const savePlaybackTimeThrottled = _.throttle((currentTime) => {
+      localStorage.setItem("vimeoPlaybackTime", currentTime.toString());
+    }, 1000);
+    if (savedTime !== null) {
+      vimeoPlayer.addEventListener("load", () => {
+        vimeoPlayer.contentWindow.postMessage(
+          {
+            method: "setCurrentTime",
+            value: parseFloat(savedTime),
+          },
+          "https://player.vimeo.com"
+        );
+      });
+    }
+    vimeoPlayer.addEventListener("timeupdate", () => {
+      const currentTime = vimeoPlayer.contentWindow.document.querySelector(
+        "video"
+      ).currentTime;
+      localStorage.setItem("vimeoPlaybackTime", currentTime.toString());
+    });
+    function playFromSavedTime() {
+      const savedTime = localStorage.getItem("vimeoPlaybackTime");
+      if (savedTime !== null) {
+        vimeoPlayer.contentWindow.postMessage(
+          {
+            method: "setCurrentTime",
+            value: parseFloat(savedTime),
+          },
+          "https://player.vimeo.com"
+        );
+        vimeoPlayer.contentWindow.postMessage(
+          {
+            method: "play",
+          },
+          "https://player.vimeo.com"
+        );
+      }
+    }
+    window.onload = playFromSavedTime;
 
 
