58610a41caa88f791e4b7136ece80a240cbb7e6c
Stefan Bielak <sbielak20@gmail.com>
2023-09-03 17:17:17 +0200

    after mentor 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,24 @@
 
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
-    const vimeoPlayer = document.getElementById("vimeo-player");
-    const savedTime = localStorage.getItem("vimeoPlaybackTime");
-    const savePlaybackTimeThrottled = _.throttle((currentTime) => {
-      localStorage.setItem("vimeoPlaybackTime", currentTime.toString());
-    }, 1000);
-    if (savedTime !== null) {
-      vimeoPlayer.addEventListener("load", () => {
-        vimeoPlayer.contentWindow.postMessage(
-          {
-            method: "setCurrentTime",
-            value: parseFloat(savedTime),
-          },
-          "https://player.vimeo.com"
-        );
-      });
-    }
-    vimeoPlayer.addEventListener("timeupdate", () => {
-      const currentTime = vimeoPlayer.contentWindow.document.querySelector(
-        "video"
-      ).currentTime;
-      localStorage.setItem("vimeoPlaybackTime", currentTime.toString());
-    });
-    function playFromSavedTime() {
-      const savedTime = localStorage.getItem("vimeoPlaybackTime");
-      if (savedTime !== null) {
-        vimeoPlayer.contentWindow.postMessage(
-          {
-            method: "setCurrentTime",
-            value: parseFloat(savedTime),
-          },
-          "https://player.vimeo.com"
-        );
-        vimeoPlayer.contentWindow.postMessage(
-          {
-            method: "play",
-          },
-          "https://player.vimeo.com"
-        );
-      }
-    }
-    window.onload = playFromSavedTime;
+import throttle from 'throttle';
+
+const vimeoPlayer = document.getElementById('vimeo-player');
+const player = new Player(vimeoPlayer);
+
+player.ready().then(() => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+  player.on('play', data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  });
+  player.on(
+    'timeupdate',
+    throttle(data => {
+      localStorage.setItem('videoplayer-current-time', data.seconds);
+    }, 1000)
+  );
+});
 
 
